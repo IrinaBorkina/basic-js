a15fc629d429e43090a43c8ea51f673b0e90d05e
Irina <irinaborkina21@gmail.com>
2020-03-13 10:11:16 +0300

    feat: add part of solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,49 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+
+        if (message === undefined || key === undefined) throw Error;
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let encryptWord = "";
+
+        for (let i = 0, j = 0; i < message.length; i++) {
+            if (alphabet.indexOf(message.charAt(i)) === -1) {
+                encryptWord += message.charAt(i);
+            } else {
+                encryptWord += alphabet.charAt((alphabet.indexOf(message.charAt(i)) + alphabet.indexOf(key.charAt(j % key.length))) % alphabet.length);
+                j++;
+            }
+        }
+
+        return encryptWord;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    decrypt(encryptedMessage, key) {
+
+        if (encryptedMessage === undefined || key === undefined) throw Error;
+
+        encryptedMessage = encryptedMessage.toUpperCase();
+        key = key.toUpperCase();
+
+        const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let decryptWord = "";
+
+        for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+            if (alphabet.indexOf(encryptedMessage.charAt(i)) === -1) {
+                decryptWord += encryptedMessage.charAt(i);
+            } else {
+                decryptWord += alphabet.charAt(((alphabet.indexOf(encryptedMessage.charAt(i)) + alphabet.length) - alphabet.indexOf(key.charAt(j % key.length))) % alphabet.length);
+                j++;
+            }
+        }
+
+        return decryptWord;
     }
 }
+
 
 module.exports = VigenereCipheringMachine;
